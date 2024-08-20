3a449ad5fa9cfe847ed7250e1417bf7064f6be40
Elsa Villarreal <elsa22@att.net>
2024-08-20 14:34:56 -0500

    finished

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            BankAccount bankAccount = new BankAccount();        
+           
+            Console.WriteLine("Enter 1 to deposit or 2 to check balance");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                Console.WriteLine($"How much would you like to deposit?");
+                int depositAmount = Console.Read();
+                bankAccount.Deposit(depositAmount);
+            }
+            if (userChoice == 2)             
+            {
+                bankAccount.GetBalance();
+               
+            }
+
+           
         }
     }
 }
